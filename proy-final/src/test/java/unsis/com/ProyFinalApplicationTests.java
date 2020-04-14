addec474150e7146f6ee9da29bcb1077a8d772c2
mireia <mire12car34@gmail.com>
2020-04-13 21:25:40 -0500

    estructura del proyecto
    la parte de backend

---
+++ proy-final/src/test/java/unsis/com/ProyFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package unsis.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
